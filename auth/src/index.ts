9a67730febb2916168a6461f5536d9db0186d477
Jattin Badrinath <jattinbadrinath@gmail.com>
2024-05-15 19:12:38 +0530

    adding mongoose and creating User model

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
 
 import { currentUserRouter } from './routes/current-user';
 import { signoutRouter } from './routes/signout';
@@ -19,6 +20,17 @@ app.use(signupRouter)
 app.get('*', async () => {
     return new NotFoundError()
 })
-app.listen(3000, () => {
-    console.log('Listening on port 3000!')
-});
+
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
+        console.log('Connected to auth DB')
+    } catch (err) {
+        console.error(err)
+    }
+    app.listen(3000, () => {
+        console.log('Listening on port 3000!')
+    })
+}
+
+start();
