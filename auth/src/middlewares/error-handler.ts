2be6755efbaae8749baa482fa370a8a2ed3879ec
Jattin Badrinath <jattinbadrinath@gmail.com>
2024-05-15 15:53:06 +0530

    Adding customError abstract class and refactoring existing code accordingly

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import { RequestValidationError } from "../errors/request-validation-errors";
+import { CustomError } from "../errors/custom-error";
 
 export const errorHandler = (
     err: Error,
@@ -7,7 +7,7 @@ export const errorHandler = (
     res: Response,
     next: NextFunction
 ) => {
-    if (err instanceof RequestValidationError) {
+    if (err instanceof CustomError) {
         return res.status(err.statusCode).send(err.serializeErrors());
     }
     return res.status(400).send({
