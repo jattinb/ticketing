bf18a78ea5d92774a3c6eda3be42c11b39faaf8b
Jattin Badrinath <jattinbadrinath@gmail.com>
2024-05-15 21:44:28 +0530

    implementing password hashing in the user model

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { Password } from "../services/password";
 
 // Interface to create user
 interface UserAttrs {
@@ -27,6 +28,13 @@ const userSchema = new mongoose.Schema({
         required: true
     }
 });
+
+userSchema.pre('save', async function (done) {
+    if (this.isModified('password')) {
+        const hashed = await Password.toHash(this.get('password'))
+        this.set('password', hashed)
+    }
+})
 
 userSchema.statics.build = (attrs: UserAttrs) => {
     return new User(attrs);
