45f75158752693895f9742b2a5289e50fdae6956
Jattin Badrinath <jattinbadrinath@gmail.com>
2024-05-15 20:37:34 +0530

    Adding Create user workflow and handling email in use error

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from 'express'
 import { body, validationResult } from 'express-validator';
 import { RequestValidationError } from '../errors/request-validation-errors';
+import { BadRequestError } from '../errors/bad-request-error';
+import { User } from '../models/user';
 
 const router = express.Router()
 
@@ -13,16 +15,21 @@ router.post('api/users/signup', [
         .isLength({ min: 4, max: 20 })
         .withMessage('Password must be between 4 and 20 characters')
 ],
-    (req: Request, res: Response) => {
+    async (req: Request, res: Response) => {
         const errors = validationResult(req)
         if (!errors.isEmpty()) {
             throw new RequestValidationError(errors.array())
         }
         const { email, password } = req.body
-        console.log('Creating User')
-        res.send({
-            'Hi': 'There'
-        })
+        const existingUser = await User.findOne({ email })
+
+        if (existingUser) {
+            throw new BadRequestError('Email in use');
+        }
+
+        const user = User.build({ email, password })
+        await user.save()
+        res.status(200).send(user);
     })
 
 export { router as signupRouter };
