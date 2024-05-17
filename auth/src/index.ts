3624c732e97df11eee6258932dd80f7ccb43c71c
Jattin Badrinath <jattinbadrinath@gmail.com>
2024-05-17 17:52:22 +0530

    refactor index.ts to app.ts

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,33 +1,6 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
+
 import mongoose from 'mongoose';
-
-import { currentUserRouter } from './routes/current-user';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { signinRouter } from './routes/signin';
-import { NotFoundError } from './errors/not-found-error';
-import cookieSession from 'cookie-session';
-
-const app = express();
-app.set('trust proxy', true)    // To trust ingress nginx
-app.use(json())
-app.use(
-    cookieSession({
-        signed: false,  // No Encryption
-        secure: true    // Only over HTTPS
-    })
-)
-
-app.use(currentUserRouter)
-app.use(signinRouter)
-app.use(signoutRouter)
-app.use(signupRouter)
-
-app.get('*', async () => {
-    return new NotFoundError()
-})
+import { app } from './app';
 
 const start = async () => {
     if (!process.env.JWT_KEY) {
