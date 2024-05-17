1f6c8d9cfd67e97a6631e266b4592de3d9414270
Jattin Badrinath <jattinbadrinath@gmail.com>
2024-05-17 23:14:43 +0530

    bug fix in extracting password and calt from existing password

--- auth/src/services/password.ts
+++ auth/src/services/password.ts
@@ -8,11 +8,11 @@ export class Password {
         const salt = randomBytes(8).toString('hex');
         const buf = (await scryptAsync(password, salt, 64)) as Buffer;
 
-        return `${buf.toString('hex')}.${salt}`;
+        return `${buf.toString('hex')}.${salt}`
     }
 
     static async compare(storedPassword: string, suppliedPassword: string) {
-        const [hashedPassword, salt] = suppliedPassword.split('.')
+        const [hashedPassword, salt] = storedPassword.split('.')
         const buf = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
 
         return buf.toString('hex') === hashedPassword
