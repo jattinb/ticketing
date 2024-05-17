6a6b1af2a8e83164e4f9a9aad01d986da2d1878a
Jattin Badrinath <jattinbadrinath@gmail.com>
2024-05-17 22:47:42 +0530

    adding signin tests

---
+++ auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,39 @@
+import request from 'supertest'
+import { app } from '../../app'
+
+it('fails when a email that does not exist is suppled', async () => {
+    await request(app)
+        .post('/api/users/signin')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(201)
+
+    await request(app)
+        .post('/api/users/signin')
+        .send({
+            email: 'test@test.com',
+            password: 'testwrongpass'
+        })
+        .expect(400)
+})
+
+it('responds with a cookie when valid credentials are supplied', async () => {
+    await request(app)
+        .post('/api/users/signin')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(201)
+
+    const response = await request(app)
+        .post('/api/users/signin')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+
+    expect(response.get('Set-Cookie')).toBeDefined()
+})
