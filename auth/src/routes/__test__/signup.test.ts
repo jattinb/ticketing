b6928ff1f28549acab8ad38d11f20b6267ec4aae
Jattin Badrinath <jattinbadrinath@gmail.com>
2024-05-17 22:47:27 +0530

    adding more signup tests

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -28,3 +28,30 @@ it('returns a 400 with an invalid password', async () => {
         })
         .expect(400)
 })
+it('disallows duplicate email', async () => {
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(201)
+    return request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(400)
+})
+
+it('sets a cookie after a successfull signup', async () => {
+    const response = await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(201)
+    expect(response.get('Set-Cookie')).toBeDefined()
+})
