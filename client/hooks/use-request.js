77727e4808ba3abbd79d3398d4ce9b22b859f535
Jattin Badrinath <jattinbadrinath@gmail.com>
2024-05-19 16:02:07 +0530

    adding logic to redirect to landing page on success

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -1,25 +1,38 @@
 import axios from 'axios';
 import { useState } from 'react';
 
-const useRequest = ({ url, method, body }) => {
+const useRequest = ({ url, method, body, onSuccess }) => {
     const [errors, setErrors] = useState(null);
 
     const doRequest = async (additionalBody = {}) => {
         try {
             setErrors(null);
             const response = await axios[method](url, { ...body, ...additionalBody });
+            if (onSuccess) {
+                onSuccess(response.data);
+            }
             return response.data;
         } catch (err) {
-            setErrors(
-                <div className="alert alert-danger">
-                    <h4>Ooops....</h4>
-                    <ul className="my-0">
-                        {err.response.data.map((error) => (
-                            <li key={error.message}>{error.message}</li>
-                        ))}
-                    </ul>
-                </div>
-            );
+            const errorResponse = err.response && err.response.data && err.response.data.errors;
+            if (errorResponse) {
+                setErrors(
+                    <div className="alert alert-danger">
+                        <h4>Ooops....</h4>
+                        <ul className="my-0">
+                            {errorResponse.map((error) => (
+                                <li key={error.message}>{error.message}</li>
+                            ))}
+                        </ul>
+                    </div>
+                );
+            } else {
+                setErrors(
+                    <div className="alert alert-danger">
+                        <h4>Ooops....</h4>
+                        <p>Something went wrong.</p>
+                    </div>
+                );
+            }
         }
     };
 
--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,22 +1,20 @@
 import { useState } from 'react';
-import axios from 'axios';
+import Router from 'next/router';
 import useRequest from '../../hooks/use-request';
 
-export default () => {
+const Signup = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const { doRequest, errors } = useRequest({
     url: '/api/users/signup',
     method: 'post',
-    body: {
-      email,
-      password
-    }
+    body: { email, password },
+    onSuccess: () => Router.push('/')
   });
-  const onSubmit = async event => {
-    event.preventDefault();
 
-    doRequest();
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await doRequest();
   };
 
   return (
@@ -44,3 +42,5 @@ export default () => {
     </form>
   );
 };
+
+export default Signup;
