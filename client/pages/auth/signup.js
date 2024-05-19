f12aa24e3109f6f1acf3faf2fce6c8f31e53ebf3
Jattin Badrinath <jattinbadrinath@gmail.com>
2024-05-19 14:35:12 +0530

    adding proper error messages in signup form

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -5,34 +5,47 @@ const SignupForm = () => {
 
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [errors, setErrors] = useState([])
 
   const onSubmit = async (event) => {
     event.preventDefault()
 
-    const response = await axios.post('/api/users/signup', {
-      email, password
-    })
+    try {
+      const response = await axios.post('/api/users/signup', {
+        email, password
+      })
 
-    console.log(email, password)
+      console.log(email, password)
+    } catch (err) {
+      setErrors(err.response.data)
+    }
   }
   return (
     <form onSubmit={onSubmit}>
       <h1>Sign Up</h1>
       <div className="form-group">
         <label>Email Address</label>
-        <input 
-          value={email} 
+        <input
+          value={email}
           onChange={event => setEmail(event.target.value)}
           className="form-control" />
       </div>
       <div className="form-group">
         <label>Password</label>
-        <input 
+        <input
           value={password}
           onChange={event => setPassword(event.target.value)}
-          type="password" 
+          type="password"
           className="form-control" />
       </div>
+      {errors.length > 0 && (<div className="alert alert-danger" >
+        <h4>Oops..</h4>
+        <ul className="my-0">
+          {errors.map(err => (
+            <li key={err.message}>{err.message}</li>
+          ))}
+        </ul>
+      </div>)}
       <button className="btn btn-primary">Sign Up</button>
     </form>
   );
