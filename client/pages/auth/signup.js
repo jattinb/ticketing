a069c42fcd841fd9a2fe3523911505d3ff73d0e4
Jattin Badrinath <jattinbadrinath@gmail.com>
2024-05-19 15:03:20 +0530

    using new custom hook and refactoring

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,25 +1,24 @@
-import { useState } from "react";
+import { useState } from 'react';
 import axios from 'axios';
+import useRequest from '../../hooks/use-request';
 
-const SignupForm = () => {
-
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [errors, setErrors] = useState([])
-
-  const onSubmit = async (event) => {
-    event.preventDefault()
-
-    try {
-      const response = await axios.post('/api/users/signup', {
-        email, password
-      })
-
-      console.log(email, password)
-    } catch (err) {
-      setErrors(err.response.data)
+export default () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { doRequest, errors } = useRequest({
+    url: '/api/users/signup',
+    method: 'post',
+    body: {
+      email,
+      password
     }
-  }
+  });
+  const onSubmit = async event => {
+    event.preventDefault();
+
+    doRequest();
+  };
+
   return (
     <form onSubmit={onSubmit}>
       <h1>Sign Up</h1>
@@ -27,28 +26,21 @@ const SignupForm = () => {
         <label>Email Address</label>
         <input
           value={email}
-          onChange={event => setEmail(event.target.value)}
-          className="form-control" />
+          onChange={e => setEmail(e.target.value)}
+          className="form-control"
+        />
       </div>
       <div className="form-group">
         <label>Password</label>
         <input
           value={password}
-          onChange={event => setPassword(event.target.value)}
+          onChange={e => setPassword(e.target.value)}
           type="password"
-          className="form-control" />
+          className="form-control"
+        />
       </div>
-      {errors.length > 0 && (<div className="alert alert-danger" >
-        <h4>Oops..</h4>
-        <ul className="my-0">
-          {errors.map(err => (
-            <li key={err.message}>{err.message}</li>
-          ))}
-        </ul>
-      </div>)}
+      {errors}
       <button className="btn btn-primary">Sign Up</button>
     </form>
   );
 };
-
-export default SignupForm
