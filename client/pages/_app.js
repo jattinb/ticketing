8e6b4b2ef824a726a942e72aa1f0f429a60710c5
Jattin Badrinath <jattinbadrinath@gmail.com>
2024-05-19 23:27:14 +0530

    adding getInitialProps for the app component

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.css'
+import buildClient from '../api/build-client'
 
-const globalCSS = ({ Component, pageProps }) => {
+const AppComponent = ({ Component, pageProps }) => {
     return <Component {...pageProps} />
 }
 
-export default globalCSS 
+AppComponent.getInitialProps = async appContext => {
+
+    const { data } = await buildClient(appContext.ctx).get('/api/users/currentuser')
+    let pageProps = {}
+    if (appContext.Component.getInitialProps) {
+        pageProps = await appContext.Component.getInitialProps(appContext.ctx)
+    }
+    return data
+}
+
+export default AppComponent
