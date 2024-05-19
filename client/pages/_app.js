5dae6603822273a00f1cf8989b40b0a3230ebdbe
Jattin Badrinath <jattinbadrinath@gmail.com>
2024-05-19 23:57:02 +0530

    creating and adding nav

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,8 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import buildClient from '../api/build-client'
+import Nav from '../components/header'
 
-const AppComponent = ({ Component, pageProps }) => {
-    return <Component {...pageProps} />
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+    return (
+        <div>
+            <Nav currentUser={currentUser} />
+            <Component {...pageProps} />
+        </div >
+    )
 }
 
 AppComponent.getInitialProps = async appContext => {
@@ -12,7 +18,10 @@ AppComponent.getInitialProps = async appContext => {
     if (appContext.Component.getInitialProps) {
         pageProps = await appContext.Component.getInitialProps(appContext.ctx)
     }
-    return data
+    return {
+        pageProps,
+        ...data
+    }
 }
 
 export default AppComponent
