3a77025dfb6f435d314409b3af42487f0849e06d
Jattin Badrinath <jattinbadrinath@gmail.com>
2024-05-20 17:31:58 +0530

    initializing and setting up tickets service

---
+++ tickets/src/app.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import { NotFoundError, errorHandler } from '@ticketingjb/common';
+import cookieSession from 'cookie-session';
+
+const app = express();
+app.set('trust proxy', true)    // To trust ingress nginx
+app.use(json())
+app.use(
+    cookieSession({
+        signed: false,  // No Encryption
+        secure: process.env.NODE_ENV !== 'test'   // Only over HTTPS
+    })
+)
+
+app.get('*', async () => {
+    return new NotFoundError()
+})
+
+app.use(errorHandler);
+
+export { app }
