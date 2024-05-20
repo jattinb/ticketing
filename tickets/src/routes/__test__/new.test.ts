798cc98fe0da531034ba34e4049eae9a6dd573bd
Jattin Badrinath <jattinbadrinath@gmail.com>
2024-05-20 23:32:50 +0530

    better defining ticket creation success test case

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import { app } from '../../app';
 import { signin } from '../../test/signin';
+import { Ticket } from '../../../models/ticket';
 
 it('has a route handler listening to /api/tickets for post requests', async () => {
   const response = await request(app).post('/api/tickets').send({});
@@ -60,11 +61,19 @@ it('returns an error if an invalid price is provided', async () => {
 });
 
 it('creates a ticket with valid inputs', async () => {
+    let tickets = await Ticket.find({})
+    const title = 'asnldsnf'
+    expect(tickets.length).toEqual(0)
     await request(app)
         .post('/api/tickets')
+        .set('Cookie', signin())
         .send({
-            title: 'asnldsnf',
+            title,
             price: 20
         })
         .expect(201)
+    tickets = await Ticket.find({});
+    expect(tickets.length).toEqual(1)
+    expect(tickets[0].price).toEqual(20)
+    expect(tickets[0].title).toEqual(title)
 });
