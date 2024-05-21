9e28bcb0531373b7132f9fe5653a6f63e45c1ef1
Jattin Badrinath <jattinbadrinath@gmail.com>
2024-05-21 20:34:35 +0530

    fixing bugs by unpdating ticketingjb/commom package version

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,26 +1,28 @@
-import express, { Request, Response } from 'express';
-import { body } from 'express-validator';
-import { requireAuth, validateRequest } from '@ticketingjb/common';
-import { Ticket } from '../models/ticket';
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import { requireAuth, validateRequest, currentUser } from "@ticketingjb/common";
+import { Ticket } from "../models/ticket";
 
 const router = express.Router();
 
 router.post(
-  '/api/tickets',
+  "/api/tickets",
   requireAuth,
   [
-    body('title').not().isEmpty().withMessage('Title is required'),
-    body('price')
+    body("title").not().isEmpty().withMessage("Title is required"),
+    body("price")
       .isFloat({ gt: 0 })
-      .withMessage('Price must be greater than 0'),
+      .withMessage("Price must be greater than 0"),
   ],
   validateRequest,
   async (req: Request, res: Response) => {
     const { title, price } = req.body;
+    const user = req.currentUser!;
+
     const ticket = Ticket.build({
       title,
       price,
-      userId: req.currentUser!.id,
+      userId: user.id,
     });
     await ticket.save();
 
