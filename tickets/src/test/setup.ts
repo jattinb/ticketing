6abbdf722c774295de70603e4d99dc66c691dd61
Jattin Badrinath <jattinbadrinath@gmail.com>
2024-05-27 19:53:48 +0530

    adding nats-wrapper mocks for testing and some formatting changes

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,26 +1,28 @@
-import { MongoMemoryServer } from 'mongodb-memory-server'
-import mongoose from 'mongoose'
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+
+jest.mock("../../nats-wrapper");
 
 let mongo: any;
 
 beforeAll(async () => {
-    process.env.JWT_KEY = 'asdf'
-    mongo = await MongoMemoryServer.create()
-    const mongoUri = mongo.getUri()
+  process.env.JWT_KEY = "asdf";
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
 
-    await mongoose.connect(mongoUri, {})
-})
+  await mongoose.connect(mongoUri, {});
+});
 
 beforeEach(async () => {
-    const collections = await mongoose.connection.db.collections()
-    for (let collection of collections) {
-        await collection.deleteMany({})
-    }
-})
+  const collections = await mongoose.connection.db.collections();
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
 
 afterAll(async () => {
-    if (mongo) {
-        await mongo.stop();
-    }
-    await mongoose.connection.close();
+  if (mongo) {
+    await mongo.stop();
+  }
+  await mongoose.connection.close();
 });
