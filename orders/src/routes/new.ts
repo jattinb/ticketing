92eba9588d899aaaaa4a5e9951984531fb81c204
Jattin Badrinath <jattinbadrinath@gmail.com>
2024-05-28 02:08:31 +0530

    working on create order logic

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,8 +1,15 @@
 import express, { Request, Response } from "express";
-import { requireAuth, validateRequest } from "@ticketingjb/common";
+import {
+  BadRequestError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+  validateRequest,
+} from "@ticketingjb/common";
 import { body } from "express-validator";
 import { Order } from "../models/order";
 import mongoose from "mongoose";
+import { Ticket } from "../models/ticket";
 
 const router = express.Router();
 
@@ -17,6 +24,29 @@ router.post(
       .withMessage("TicketId must be provided"),
   ],
   async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+
+    // Find the ticket the user is trying yo order in the db
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    // Make sure the ticket is not already reserved
+    // Run query to look at all orders, find an order where the ticket
+    // is the ticket we just found  *and* the orders status is *not* cancelled.
+    // If we find an order from that means the ticket *is* reserved
+    const isReserved = await ticket.isReserved();
+    if (isReserved) {
+      throw new BadRequestError("Ticket is already reserved");
+    }
+    // Calculate an expiration date for this order
+
+    // Build the order and save it to the db
+    // Order.build({
+    // })
+
+    // Publish an event to indicate an order was created
     res.send({});
   }
 );
