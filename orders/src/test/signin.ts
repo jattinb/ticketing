17bcdfbeab7de751ef782de39df99204d6768efd
Jattin Badrinath <jattinbadrinath@gmail.com>
2024-05-28 03:21:40 +0530

    adding some test setup

---
+++ orders/src/test/signin.ts
@@ -0,0 +1,20 @@
+import { app } from '../app'
+import request from 'supertest'
+import jwt from 'jsonwebtoken'
+import mongoose from 'mongoose'
+
+
+export const signin = () => {
+    const payload = {
+        id: new mongoose.Types.ObjectId().toHexString(),
+        email: 'test@test.com',
+      };
+
+    const token = jwt.sign(payload, process.env.JWT_KEY!)
+    const session = { jwt: token}
+    const sessionJSON = JSON.stringify(session)
+    const base64 = Buffer.from(sessionJSON).toString('base64')
+
+    return [`session=${base64}`]
+
+}
