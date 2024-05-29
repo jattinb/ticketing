c721fa2d26dca5cd3edc305b3dd77b195617a317
Jattin Badrinath <jattinbadrinath@gmail.com>
2024-05-29 22:00:08 +0530

    adding tests

--- tickets/src/events/listeners/__test__/order-created-listener.test.ts
+++ tickets/src/events/listeners/__test__/order-created-listener.test.ts
@@ -2,6 +2,7 @@ import { OrderCreatedEvent, OrderStatus } from "@ticketingjb/common";
 import { Ticket } from "../../../models/ticket";
 import { natsWrapper } from "../../../nats-wrapper";
 import { OrderCreatedListener } from "../order-created-listener";
+import { Message } from "node-nats-streaming";
 import mongoose from "mongoose";
 
 const setup = async () => {
@@ -32,3 +33,20 @@ const setup = async () => {
 
   return { listener, ticket, data, msg };
 };
+
+it("sets the userId of the ticket", async () => {
+  const { listener, ticket, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  const updatedTicket = await Ticket.findById(ticket.id);
+
+  expect(updatedTicket!.orderId).toEqual(data.id);
+});
+
+it("acks the message", async () => {
+  const { listener, ticket, data, msg } = await setup();
+  await listener.onMessage(data, msg);
+
+  expect(msg.ack).toHaveBeenCalled();
+});
