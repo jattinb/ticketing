26eb2ba5fee6b7a04f79dece16f3208b2a823312
Jattin Badrinath <jattinbadrinath@gmail.com>
2024-06-01 23:31:49 +0530

    adding orders model

---
+++ payments/src/models/order.ts
@@ -0,0 +1,67 @@
+import mongoose from "mongoose";
+import { OrderStatus } from "@ticketingjb/common";
+import { updateIfCurrentPlugin } from "mongoose-update-if-current";
+export { OrderStatus };
+
+interface OrderAttrs {
+  id: string;
+  status: OrderStatus;
+  version: Number;
+  userId: string;
+  price: Number;
+}
+
+interface OrderDoc extends mongoose.Document {
+  status: OrderStatus;
+  version: Number;
+  userId: string;
+  price: Number;
+}
+
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc;
+}
+
+const orderSchema = new mongoose.Schema(
+  {
+    status: {
+      type: String,
+      required: true,
+      enum: Object.values(OrderStatus),
+      default: OrderStatus.Created,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+orderSchema.set("versionKey", "version");
+orderSchema.plugin(updateIfCurrentPlugin);
+
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order({
+    _id: attrs.id,
+    version: attrs.version,
+    price: attrs.price,
+    userId: attrs.userId,
+    status: attrs.status,
+  });
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>("Order", orderSchema);
+
+export { Order };
