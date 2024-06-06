1bf81190bf435679ff452a02da826a959108db4c
Jattin Badrinath <32822529+jattinb@users.noreply.github.com>
2024-06-07 01:10:39 +0530

    [Frontend] Adding multiple features to the frontend (#3)

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -4,35 +4,27 @@ import { useState } from 'react';
 const useRequest = ({ url, method, body, onSuccess }) => {
     const [errors, setErrors] = useState(null);
 
-    const doRequest = async (additionalBody = {}) => {
+    const doRequest = async (props = {}) => {
         try {
             setErrors(null);
-            const response = await axios[method](url, { ...body, ...additionalBody });
+            const response = await axios[method](url, { ...body, ...props });
+
             if (onSuccess) {
                 onSuccess(response.data);
             }
+
             return response.data;
         } catch (err) {
-            const errorResponse = err.response && err.response.data && err.response.data.errors;
-            if (errorResponse) {
-                setErrors(
-                    <div className="alert alert-danger">
-                        <h4>Ooops....</h4>
-                        <ul className="my-0">
-                            {errorResponse.map((error) => (
-                                <li key={error.message}>{error.message}</li>
-                            ))}
-                        </ul>
-                    </div>
-                );
-            } else {
-                setErrors(
-                    <div className="alert alert-danger">
-                        <h4>Ooops....</h4>
-                        <p>Something went wrong.</p>
-                    </div>
-                );
-            }
+            setErrors(
+                <div className="alert alert-danger">
+                    <h4>Oops...</h4>
+                    <ul className="my-0">
+                        {err.response.data.map((err, index) => (
+                            <li key={index}>{err.message}</li>
+                        ))}
+                    </ul>
+                </div>
+            );
         }
     };
 
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,27 +1,35 @@
-import 'bootstrap/dist/css/bootstrap.css'
-import buildClient from '../api/build-client'
-import Nav from '../components/header'
+import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
+import Header from '../components/header';
 
 const AppComponent = ({ Component, pageProps, currentUser }) => {
     return (
         <div>
-            <Nav currentUser={currentUser} />
-            <Component {...pageProps} />
-        </div >
-    )
-}
+            <Header currentUser={currentUser} />
+            <div className="container">
+                <Component currentUser={currentUser} {...pageProps} />
+            </div>
+        </div>
+    );
+};
 
-AppComponent.getInitialProps = async appContext => {
+AppComponent.getInitialProps = async (appContext) => {
+    const client = buildClient(appContext.ctx);
+    const { data } = await client.get('/api/users/currentuser');
 
-    const { data } = await buildClient(appContext.ctx).get('/api/users/currentuser')
-    let pageProps = {}
+    let pageProps = {};
     if (appContext.Component.getInitialProps) {
-        pageProps = await appContext.Component.getInitialProps(appContext.ctx)
+        pageProps = await appContext.Component.getInitialProps(
+            appContext.ctx,
+            client,
+            data.currentUser
+        );
     }
+
     return {
         pageProps,
-        ...data
-    }
-}
+        ...data,
+    };
+};
 
-export default AppComponent
+export default AppComponent;
