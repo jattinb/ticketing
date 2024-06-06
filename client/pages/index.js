38d04135e04d68fea15deb397dbddae5a95c405c
Jattin Badrinath <jattinbadrinath@gmail.com>
2024-06-06 21:47:46 +0530

    displaying ticketis on the landing page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,14 +1,41 @@
-import React from 'react';
-import buildClient from '../api/build-client';
+import Link from 'next/link';
 
-const LandingPage = ({ currentUser }) => {
-    return currentUser ? <h1>You are Signed In</h1> : <h1>You are NOT Signed In</h1>
+const LandingPage = ({ currentUser, tickets }) => {
+    const ticketList = tickets.map((ticket) => {
+        return (
+            <tr key={ticket.id}>
+                <td>{ticket.title}</td>
+                <td>{ticket.price}</td>
+                <td>
+                    <Link href="/tickets/[ticketId]" as={`/tickets/${ticket.id}`}>
+                        View
+                    </Link>
+                </td>
+            </tr>
+        );
+    });
+
+    return (
+        <div>
+            <h1>Tickets</h1>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Price</th>
+                        <th>Link</th>
+                    </tr>
+                </thead>
+                <tbody>{ticketList}</tbody>
+            </table>
+        </div>
+    );
 };
 
-LandingPage.getInitialProps = async (context) => {
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+    const { data } = await client.get('/api/tickets');
 
-    const { data } = await buildClient(context).get('/api/users/currentuser')
-    return data
-}
+    return { tickets: data };
+};
 
 export default LandingPage;
