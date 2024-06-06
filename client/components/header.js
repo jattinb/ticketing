bf298cc383375ca6ef550ecf0fb1ced5017e7e21
Jattin Badrinath <jattinbadrinath@gmail.com>
2024-06-06 22:18:00 +0530

    fixing nav

--- client/components/header.js
+++ client/components/header.js
@@ -1,27 +1,31 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
-const Nav = ({ currentUser }) => {
+export default ({ currentUser }) => {
     const links = [
-        !currentUser && { label: 'Sign Up', href: 'auth/signup' },
-        !currentUser && { label: 'Sign In', href: 'auth/signin' },
-        currentUser && { label: 'Sign Out', href: 'auth/signout' }
-    ].filter(linkConfig => linkConfig)
+        !currentUser && { label: 'Sign Up', href: '/auth/signup' },
+        !currentUser && { label: 'Sign In', href: '/auth/signin' },
+        currentUser && { label: 'Sign Out', href: '/auth/signout' },
+    ]
+        .filter((linkConfig) => linkConfig)
         .map(({ label, href }) => {
-            return (<li key={href} className='nav-item'>
-                <Link href={href} className='nav-link'>
-                    {label}
-                </Link>
-            </li>)
-        })
-    return <nav className="navbar navbar-light bg-light">
-        <Link className="navbar-brand" href="/">
-            Ticketing
-        </Link>
-        <div className="d-flex justify-content-end">
-            <ul className="nav d-flex align-items-center">{
-                links
-            }</ul></div>
-    </nav>
-}
+            return (
+                <li key={href} className="nav-item">
+                    <Link className="nav-link" href={href}>
+                        {label}
+                    </Link>
+                </li>
+            );
+        });
 
-export default Nav
+    return (
+        <nav className="navbar navbar-light bg-light">
+            <Link className="navbar-brand" href="/">
+                GitTix
+            </Link>
+
+            <div className="d-flex justify-content-end">
+                <ul className="nav d-flex align-items-center">{links}</ul>
+            </div>
+        </nav>
+    );
+};
